d4181f7c0a77199fc4c2c3b5fd24180b7b8f7527
Brun0HM <brunodev41@gmail.com>
2024-08-27 11:54:12 -0300

    Iniciando projeto MVC com a lisatgem de Alunos

---
+++ PrimeiroMVC/Controllers/AlunoController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimeiroMVC.Models;
+
+namespace PrimeiroMVC.Controllers
+{
+    public class AlunoController : Controller
+    {
+        public IActionResult Index()
+        {
+            Aluno a1 = new Aluno(1, "Bruno Henrique", "Bruno.lindo@gmail.com", 696969);
+            Aluno a2 = new Aluno(2, "Thiago Mazzi", "Mazzi.lindo@gmail.com", 348934);
+            Aluno a3 = new Aluno(3, "Ryan", "Ryan.lindo@gmail.com", 775533);
+            Aluno a4 = new Aluno(4, "Jones", "Jones.lindo@gmail.com", 990077);
+            Aluno a5 = new Aluno(5, "Bryan", "Rato.lindo@gmail.com", 326946);
+
+            //Criar uma lista de Alunos
+            List<Aluno> ListaAlunos = new List<Aluno>();
+            //Adicionar os objetos Alunos na lista
+            ListaAlunos.Add(a1);
+            ListaAlunos.Add(a2);
+            ListaAlunos.Add(a3);
+            ListaAlunos.Add(a4);
+            ListaAlunos.Add(a5);
+            // Enviando a Lista de Alunos´por parametro na view Index
+            return View(ListaAlunos);
+        }
+        public IActionResult Cadastrar() { return View(); }
+    }
+}
---
+++ PrimeiroMVC/Models/Aluno.cs
@@ -0,0 +1,20 @@
+﻿namespace PrimeiroMVC.Models
+{
+    public class Aluno
+    {
+        public int AlunoId { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int RM { get; set; }
+
+        //Modificar o método construtor
+        // Inserir os dados que vieram por parametros nos atributos do Objeto
+        public Aluno(int id, string nome, string email, int rm)
+        {
+            this.AlunoId = id;
+            this.Nome = nome;
+            this.Email = email;
+            this.RM = rm;
+        }
+    }
+}
